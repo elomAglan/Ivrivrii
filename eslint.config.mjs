06a081c826c9cc3a4e0297cb2d010aa3b56f5e33
elomAglan <blinklintelobp@gmail.com>
2025-08-10 10:33:46 +0000

    good

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,18 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    // Ajout d'une configuration pour désactiver des règles
+    rules: {
+      // Désactive la règle qui empêche les apostrophes et guillemets non échappés
+      "react/no-unescaped-entities": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
